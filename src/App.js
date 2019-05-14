aa29731463c268a3808207298170f4821119f6a1
Zoran Lazic <zoran@websupercell.com>
2019-05-14 20:22:23 +0200

    added google analytics

--- src/App.js
+++ src/App.js
@@ -1,22 +1,27 @@
 import React from 'react';
+import ReactGA from 'react-ga';
+
 import mainCoverImage from './assets/PistonVault.png';
 import './App.css';
+
+ReactGA.initialize(process.env.REACT_APP_GA_TRACKING_ID);
 
 const SHOW_VIDEO = process.env.REACT_APP_SHOW_VIDEO === 'true';
 
 function App() {
-  return (
-    <div className="App">
-        {SHOW_VIDEO && (
-            <video src="./piston-video-presentation.mp4" poster={mainCoverImage} autoPlay controls></video>
-        )}
-        {!SHOW_VIDEO && (
-            <div className="image">
-                <img src={mainCoverImage} />
-            </div>
-        )}
-    </div>
-  );
+	ReactGA.pageview('/');
+	return (
+		<div className="App">
+			{SHOW_VIDEO && (
+				<video src="./piston-video-presentation.mp4" poster={mainCoverImage} autoPlay controls></video>
+			)}
+			{!SHOW_VIDEO && (
+				<div className="image">
+					<img src={mainCoverImage} />
+				</div>
+			)}
+		</div>
+	);
 }
 
 export default App;
