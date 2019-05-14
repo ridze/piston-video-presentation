43ee7ca545ca9624f2a80c5e854096cb1c7dfb73
Zoran Lazic <zoran@websupercell.com>
2019-05-14 20:17:48 +0200

    added check for turning off video

--- src/App.js
+++ src/App.js
@@ -2,10 +2,19 @@ import React from 'react';
 import mainCoverImage from './assets/PistonVault.png';
 import './App.css';
 
+const SHOW_VIDEO = process.env.REACT_APP_SHOW_VIDEO === 'true';
+
 function App() {
   return (
     <div className="App">
-        <video src="./piston-video-presentation.mp4" poster={mainCoverImage} autoPlay controls></video>
+        {SHOW_VIDEO && (
+            <video src="./piston-video-presentation.mp4" poster={mainCoverImage} autoPlay controls></video>
+        )}
+        {!SHOW_VIDEO && (
+            <div className="image">
+                <img src={mainCoverImage} />
+            </div>
+        )}
     </div>
   );
 }
