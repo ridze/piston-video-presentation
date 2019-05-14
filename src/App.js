23926f34ea7cf7e3e312c17ae8ecce5197a031fc
Stefan Djordjevic <stefan.djordjevic@websupercell.com>
2019-05-14 18:59:41 +0200

    fix image border

--- src/App.js
+++ src/App.js
@@ -6,7 +6,9 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={mainCoverImage} className="App-logo" alt="logo" />
+        <span className="App-logo-wrapper">
+           <img src={mainCoverImage} className="App-logo" alt="logo" />
+        </span>
       </header>
       <video src="./piston-video-presentation.mp4" width="400" height="300" controls></video>
     </div>
