64115d4b029d5d8afca077f21cfcc96703184090
Zoran Lazic <zoran@websupercell.com>
2019-05-14 19:27:48 +0200

    save

--- src/App.js
+++ src/App.js
@@ -5,12 +5,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <span className="App-logo-wrapper">
-           <img src={mainCoverImage} className="App-logo" alt="logo" />
-        </span>
-      </header>
-      <video src="./piston-video-presentation.mp4" width="400" height="300" controls></video>
+        <video src="./piston-video-presentation.mp4" poster={mainCoverImage} autoPlay controls></video>
     </div>
   );
 }
