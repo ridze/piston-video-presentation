4858a427dd3ff6cb0deb82051608bc81fa8ec613
Zoran Lazic <zoran@websupercell.com>
2019-05-14 21:22:15 +0200

    added preload

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
 	return (
 		<div className="App">
 			{SHOW_VIDEO && (
-				<video src="./piston-video-presentation.mp4" poster={mainCoverImage} autoPlay controls></video>
+				<video src="./piston-video-presentation.mp4" preload poster={mainCoverImage} autoplay controls></video>
 			)}
 			{!SHOW_VIDEO && (
 				<div className="image">
