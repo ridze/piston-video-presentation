c8c2deccdb275754f42fcfae9c4d047d3b757917
Stefan Djordjevic <stefan.djordjevic@websupercell.com>
2019-05-14 18:39:04 +0200

    add project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
+import mainCoverImage from './assets/piston-video-cover-img.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={mainCoverImage} className="App-logo" alt="logo" />
       </header>
+      <video src="./assets/piston-video-presentation.mp4" width="400" height="300" controls></video>
     </div>
   );
 }
